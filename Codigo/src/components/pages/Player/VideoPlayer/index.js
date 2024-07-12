672b08c49a13c75dd3f689a4815d65dd940feb03
Rafael Estanislau <rafalevel100@hotmail.com>
2024-07-12 17:57:26 -0300

    atualizando buffer para pegar partial content

--- Codigo/src/components/pages/Player/VideoPlayer/index.js
+++ Codigo/src/components/pages/Player/VideoPlayer/index.js
@@ -1,65 +1,48 @@
-import React, { useRef, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 
-const VideoPlayer = () => {
-  const { id } = useParams();
-  const videoRef = useRef(null);
+const VideoPlayer = ({ id }) => {
+  const videoUrl =
+    "exampleUrl";
+  const [isBuffering, setIsBuffering] = useState(true);
+  const [videoSource, setVideoSource] = useState();
 
   useEffect(() => {
-    const video = videoRef.current;
-
-    const handleLoadedMetadata = () => {
-      console.log("Metadata loaded");
-    };
-
-    const handleProgress = () => {
-      const buffered = video.buffered;
-      const bufferedTimeRanges = [];
-
-      for (let i = 0; i < buffered.length; i++) {
-        bufferedTimeRanges.push([buffered.start(i), buffered.end(i)]);
-      }
-
-      console.log("Buffered Time Ranges: ", bufferedTimeRanges);
-    };
-
-    const handlePlaying = () => {
-      console.log("Video is playing");
-    };
-
-    const handlePause = () => {
-      console.log("Video is paused");
-    };
-
-    const handleWaiting = () => {
-      console.log("Video is waiting for more data");
-    };
-
-    video.addEventListener("loadedmetadata", handleLoadedMetadata);
-    video.addEventListener("progress", handleProgress);
-    video.addEventListener("playing", handlePlaying);
-    video.addEventListener("pause", handlePause);
-    video.addEventListener("waiting", handleWaiting);
-
-    return () => {
-      video.removeEventListener("loadedmetadata", handleLoadedMetadata);
-      video.removeEventListener("progress", handleProgress);
-      video.removeEventListener("playing", handlePlaying);
-      video.removeEventListener("pause", handlePause);
-      video.removeEventListener("waiting", handleWaiting);
-    };
+    (async () => {
+      let url = await fetchVideoSegments(videoUrl);
+      setVideoSource(url);
+    })();
   }, []);
 
-  // URL pública de exemplo para o vídeo
-  const videoUrl = "https://skillforgesoftware.s3.us-east-2.amazonaws.com/bucket/FALLEN+PASSANDO+DICAS+de+SITUA%C3%87%C3%95ES+que+TODOS+PLAYERS+DE+CS%EF%BC%9AGO+DEVERIAM+SABER!!.mp4";
+
+  const fetchVideoSegments = async (url) => {
+    try {
+      const response = await fetch(url, {
+        headers: {
+          Range: `bytes=${108199936}-`,
+        },
+      });
+      const arrayBuffer = await response.arrayBuffer();
+      const blob = new Blob([new Uint8Array(arrayBuffer)]);
+      const blobUrl = URL.createObjectURL(blob);
+      console.log("Fetched array buffer:", arrayBuffer);
+      return blobUrl;
+    } catch (error) {
+      console.error("Error fetching video segments:", error);
+      return [];
+    }
+  };
 
   return (
     <div>
       <h1>Video Player for Video ID: {id}</h1>
-      <video ref={videoRef} width="100%" height="720px" controls>
-        <source src={videoUrl} type="video/mp4" />
+      <video src={videoSource} width="100%" height="720px" controls>
         Your browser does not support the video tag.
       </video>
+      {isBuffering && (
+        <div>
+          <p>Buffering...</p>
+        </div>
+      )}
     </div>
   );
 };
